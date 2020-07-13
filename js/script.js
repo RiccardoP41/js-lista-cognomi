3139aa425b052b4381de11639647c6118ab5e69b
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-13 16:05:23 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -3,18 +3,27 @@
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
-// Creare array
+// ***Creare array
 var lista = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
-// Chiedere cognome
+// ***Chiedere cognome
 var elemento = prompt("inserisci il cognome");
 
-// Inserirlo nella lista
-var elementoCorretto = lista.push(elemento.charAt(0).toUpperCase() + elemento.slice(1).toLowerCase());
+// ***Inserirlo nella lista
 
-// Ordinare la lista alfabeticamente e stamparla in una lista ordinata
+// lista.push(elemento.charAt(0).toUpperCase() + elemento.slice(1).toLowerCase());    \\TROVATO L'ERRORE
+
+var maiuscola = elemento.charAt(0).toUpperCase();
+var minuscole = elemento.slice(1).toLowerCase();
+elemento = maiuscola + minuscole;
+lista.push(elemento);
+
+// ***Ordinare la lista alfabeticamente e stamparla in una lista ordinata
 lista.sort();
 for (var i = 0; i < lista.length; i++) {
     document.getElementById("cognome").innerHTML += "<li>" + lista[i] + "</li>";
 }
-// posizione umana dell'elemento
+
+// ***posizione umana dell'elemento
+var posizione = lista.indexOf(elemento);
+document.getElementById("posizione").innerHTML = " Si trova nella posizione " + (posizione + 1);
