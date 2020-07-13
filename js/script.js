a29fd7481f94848b9e51fcd93338dd4fa89690ae
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-13 16:14:16 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -10,20 +10,26 @@ var lista = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 var elemento = prompt("inserisci il cognome");
 
 // ***Inserirlo nella lista
-
-// lista.push(elemento.charAt(0).toUpperCase() + elemento.slice(1).toLowerCase());    \\TROVATO L'ERRORE
-
+// lista.push(elemento.charAt(0).toUpperCase() + elemento.slice(1).toLowerCase());    \\TROVATO L'INTOPPO
 var maiuscola = elemento.charAt(0).toUpperCase();
 var minuscole = elemento.slice(1).toLowerCase();
 elemento = maiuscola + minuscole;
 lista.push(elemento);
 
-// ***Ordinare la lista alfabeticamente e stamparla in una lista ordinata
+
+// ***Ordinare la lista alfabeticamente
 lista.sort();
+
+// ***Stamparla in una lista ordinata con posizione
 for (var i = 0; i < lista.length; i++) {
     document.getElementById("cognome").innerHTML += "<li>" + lista[i] + "</li>";
+    if (elemento == lista [i]) {
+        document.getElementById("posizione").innerHTML = " Si trova nella posizione " + (i + 1);
+    }
 }
 
-// ***posizione umana dell'elemento
-var posizione = lista.indexOf(elemento);
-document.getElementById("posizione").innerHTML = " Si trova nella posizione " + (posizione + 1);
+
+// ***posizione umana dell'elemento con indexOf
+
+// var posizione = lista.indexOf(elemento);
+// document.getElementById("posizione").innerHTML = " Si trova nella posizione " + (posizione + 1);
